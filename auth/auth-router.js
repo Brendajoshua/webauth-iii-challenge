69b49e072bff3cc8e4737542dbef092f0214478e
Brenda Joshua <brendajosh.bj@gmail.com>
2019-11-13 17:33:54 +0300

    added register and login endpoints

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -8,6 +8,32 @@ router.post('/register', (req, res) => {
     let user = req.body;
     const hash = bcrypt.hashSync(user.password, 10);
     user.password = hash;
-})
+
+    Users.add(user)
+    .then(saved => {
+        res.status(201).json(saved);
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'cannot add the user' });
+    });
+});
+
+router.post('/login', (req, res) => {
+    let {username, password } = req.body;
+
+    Users.findBy({ username })
+    .first()
+    .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+            const token = generateToken(user);
+            res.status(200).json({ message: `Welcome ${user.username}!`, token });
+        } else {
+            res.status(401).json({ message: 'Invalid credentials' });
+        }
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
